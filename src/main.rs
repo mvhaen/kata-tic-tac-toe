10c9423d57fd568476e45b04a0225c05536ad0bd
Michael Voorhaen <michael.voorhaen@gmail.com>
2023-10-29 14:09:12 +0100

    split up into multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,22 @@
 mod player;
 mod board;
+mod input;
+mod output;
 
-use std::io::{BufRead, BufReader};
+use std::io::BufReader;
 use player::{Player, player_to_string};
 use board::Board;
+use output::print_board;
+use input::get_input;
 
 fn main() {
   play();
 }
 
-fn get_input(player: &Player, reader: &mut dyn BufRead) -> Result<(usize, usize), String> {
-  fn get_input_for(prompt: &str, player: &Player, reader: &mut dyn BufRead) -> Result<usize, String> {
-    println!("Please enter {} for player {}: ", prompt, player_to_string(*player));
-    let mut input = String::new();
-    reader.read_line(&mut input).map_err(|e| e.to_string())?;
-    match input.trim().parse::<usize>() {
-        Ok(num) if num <= 2 => {
-            return Ok(num);
-        }
-        _ => return Err(format!("Invalid input for {}!", prompt)),
-    }
-  }
-
-  let row = get_input_for("row", player, reader);
-  if row.is_err() {
-      return Err(row.err().unwrap());
-  }
-
-  let col = get_input_for("col", player, reader);
-  if col.is_err() {
-    return Err(col.err().unwrap());
-  }
-
-  Ok((row.unwrap(), col.unwrap()))
-}
-
-
-
-fn print_board(board: &Board) {
-  for row in 0..3 {
-    for col in 0..3 {
-      let cell = board.get_cell(row, col);
-      let cell_str = match cell {
-        Some(player) => player_to_string(player),
-        None => " ".to_string(),
-      };
-      print!("{}", cell_str);
-      if col < 2 {
-        print!("|");
-      }
-    }
-    println!();
-    if row < 2 {
-      println!("-----");
-    }
-  }
-}
-
 fn play() {
   // initialize game state
-let mut board = Board::new();
-let mut current_player = Player::X;
+  let mut board = Board::new();
+  let mut current_player = Player::X;
 
   // main game loop
   loop {
@@ -105,25 +61,3 @@ let mut current_player = Player::X;
   }
 }
 
-
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn it_returns_valid_input_given_valid_input() {
-    let mut reader = BufReader::new("1\n2\n".as_bytes());
-    let (row, col) = get_input(&Player::X, &mut reader).unwrap();
-    assert_eq!(1, row);
-    assert_eq!(2, col);
-  }
-
-  #[test]
-  fn it_returns_an_error_given_an_invalid_row() {
-    let mut reader = BufReader::new("3\n1\n".as_bytes());
-    let result = get_input(&Player::X, &mut reader);
-    assert!(result.is_err());
-    assert_eq!("Invalid input for row!", result.err().unwrap());
-  }
-}
