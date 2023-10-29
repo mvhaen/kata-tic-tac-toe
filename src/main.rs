56ad02631e2819a9b0afb7bdd05c0ffe02ec7be0
Michael Voorhaen <michael.voorhaen@gmail.com>
2023-10-29 13:06:54 +0100

    game loop, input processing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,65 @@
+use std::io::{BufRead, BufReader};
+
 fn main() {
-  println!("{}", hello_world());
+  play();
 }
 
-fn hello_world() -> String {
-  return "Hello World!".to_string();
+/// function that reads input from the cmd line and returns a tuple of (row, col)
+fn get_input(player: &Player, reader: &mut dyn BufRead) -> Result<(usize, usize), String> {
+  println!("Please enter row for player {}: ", player_to_string(*player));
+  let mut input = String::new();
+  reader.read_line(&mut input).expect("Failed to read line");
+  let row = match input.trim().parse::<usize>() {
+    Ok(num) => num,
+    Err(_) => {
+      return Err("Invalid input for row!".to_string());
+    }
+  };
+  if row > 2 {
+    return Err("Invalid input for row!".to_string());
+  }
+
+  println!("Please enter col for player {}: ", player_to_string(*player));
+  let mut input = String::new();
+  reader.read_line(&mut input).expect("Failed to read line");
+  let col = match input.trim().parse::<usize>() {
+    Ok(num) => num,
+    Err(_) => {
+      return Err("Invalid input for col!".to_string());
+    }
+  };
+  if col > 2 {
+    return Err("Invalid input for col!".to_string());
+  }
+
+  return Ok((row, col));
 }
 
-fn validate_input(input: &str) -> bool {
-  return input == "X" || input == "O";
+fn player_to_string(player: Player) -> String {
+  match player {
+    Player::X => "X".to_string(),
+    Player::O => "O".to_string(),
+  }
+}
+
+fn print_board(board: &Board) {
+  for row in 0..3 {
+    for col in 0..3 {
+      let cell = board.get_cell(row, col);
+      let cell_str = match cell {
+        Some(player) => player_to_string(player),
+        None => " ".to_string(),
+      };
+      print!("{}", cell_str);
+      if col < 2 {
+        print!("|");
+      }
+    }
+    println!();
+    if row < 2 {
+      println!("-----");
+    }
+  }
 }
 
 #[derive(Copy, Clone, PartialEq)]
@@ -15,6 +67,54 @@ fn validate_input(input: &str) -> bool {
 enum Player {
   X,
   O,
+}
+
+fn play() {
+  // initialize game state
+let mut board = Board::new();
+let mut current_player = Player::X;
+
+  // main game loop
+  loop {
+      // print board
+      print_board(&board);
+
+      // get input from player from stdin
+      let mut reader = BufReader::new(std::io::stdin());
+      let (row, col) = match get_input(&current_player, &mut reader) {
+          Ok((row, col)) => (row, col),
+          Err(err) => {
+              println!("Error: {}", err);
+              continue;
+          }
+      };
+
+      // update board
+      let result = board.set_cell(row, col, current_player);
+
+      if result.is_err() {
+          println!("Error: {}", result.err().unwrap());
+          continue;
+      }
+
+      // check if player has won
+      if board.has_won(current_player) {
+          println!("{} has won!", player_to_string(current_player));
+          break;
+      }
+
+      // check if board is full
+      if board.is_full() {
+          println!("The game is a tie!");
+          break;
+      }
+
+      // switch player
+      current_player = match current_player {
+          Player::X => Player::O,
+          Player::O => Player::X,
+      };
+  }
 }
 
 struct Board {
@@ -36,6 +136,17 @@ impl Board {
         }
         self.cells[row][col] = Some(player);
         Ok(())
+    }
+
+    fn is_full(&self) -> bool {
+        for row in self.cells.iter() {
+            for cell in row.iter() {
+                if cell.is_none() {
+                    return false;
+                }
+            }
+        }
+        true
     }
 
     fn has_won(&self, player: Player) -> bool {
@@ -69,23 +180,6 @@ impl Board {
 #[cfg(test)]
 mod tests {
   use super::*;
-
-  #[test] 
-  fn it_returns_hello_world() {
-      assert_eq!("Hello World!", hello_world());
-  }
-
-  #[test]
-  fn it_returns_true_if_input_is_x_or_o() {
-    assert_eq!(true, validate_input("X"));
-    assert_eq!(true, validate_input("O"));
-  }
-
-  #[test] 
-  fn it_returns_false_if_input_is_not_x_or_o() {
-    assert_eq!(false, validate_input("A"));
-    assert_eq!(false, validate_input("B"));
-  }
 
   #[test]
   fn it_creates_an_empty_board() {
@@ -166,5 +260,51 @@ mod tests {
     assert_eq!(Some(Player::X), board.get_cell(0, 0));
     assert_eq!(Ok(()), board.set_cell(1, 2, Player::O));
     assert_eq!(Some(Player::O), board.get_cell(1, 2));
+  }
+
+  #[test]
+  fn it_returns_true_if_the_board_is_full() {
+    // write test code that checks if the board is full
+    let mut board = Board::new();
+    assert_eq!(Ok(()), board.set_cell(0, 0, Player::X));
+    assert_eq!(Ok(()), board.set_cell(0, 1, Player::X));
+    assert_eq!(Ok(()), board.set_cell(0, 2, Player::X));
+    assert_eq!(Ok(()), board.set_cell(1, 0, Player::X));
+    assert_eq!(Ok(()), board.set_cell(1, 1, Player::X));
+    assert_eq!(Ok(()), board.set_cell(1, 2, Player::X));
+    assert_eq!(Ok(()), board.set_cell(2, 0, Player::X));
+    assert_eq!(Ok(()), board.set_cell(2, 1, Player::X));
+    assert_eq!(Ok(()), board.set_cell(2, 2, Player::X));
+    assert_eq!(true, board.is_full());
+  }
+
+  #[test]
+  fn it_returns_false_if_the_board_is_full() {
+    // write test code that checks if the board is full
+    let mut board = Board::new();
+    assert_eq!(false, board.is_full());
+    assert_eq!(Ok(()), board.set_cell(0, 0, Player::X));
+    assert_eq!(false, board.is_full());
+    assert_eq!(Ok(()), board.set_cell(0, 1, Player::X));
+    assert_eq!(false, board.is_full());
+    assert_eq!(Ok(()), board.set_cell(0, 2, Player::X));
+    assert_eq!(Ok(()), board.set_cell(2, 2, Player::O));
+    assert_eq!(false, board.is_full());
+  }
+
+  #[test]
+  fn it_returns_valid_input_given_valid_input() {
+    let mut reader = BufReader::new("1\n2\n".as_bytes());
+    let (row, col) = get_input(&Player::X, &mut reader).unwrap();
+    assert_eq!(1, row);
+    assert_eq!(2, col);
+  }
+
+  #[test]
+  fn it_returns_an_error_given_an_invalid_row() {
+    let mut reader = BufReader::new("3\n1\n".as_bytes());
+    let result = get_input(&Player::X, &mut reader);
+    assert!(result.is_err());
+    assert_eq!("Invalid input for row!", result.err().unwrap());
   }
 }
