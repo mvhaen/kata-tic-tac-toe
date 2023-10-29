49f407b83724e3f41a91280c5bc9bdaed78908b7
Michael Voorhaen <michael.voorhaen@gmail.com>
2023-10-29 10:05:59 +0100

    player definition, update the board based on it, add an has_won function

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,47 @@ fn hello_world() -> String {
   return "Hello World!".to_string();
 }
 
-type Board = [[Option<char>; 3]; 3];
+type Board = [[Option<Player>; 3]; 3];
+
+#[derive(Copy, Clone, PartialEq)]
+#[derive(Debug)]
+enum Player {
+  X,
+  O,
+}
 
 fn create_board() -> Board {
-  return [[None, None, None], [None, None, None], [None, None, None]];
+  return [[None; 3]; 3];
 }
 
 fn validate_input(input: &str) -> bool {
   return input == "X" || input == "O";
+}
+
+fn has_won(board: Board, player: Player) -> bool {
+  // check rows
+  for row in board {
+    if row.iter().all(|&x| x == Some(player)) {
+      return true;
+    }
+  }
+
+  // check columns
+  for i in 0..3 {
+    if board[0][i] == Some(player) && board[1][i] == Some(player) && board[2][i] == Some(player) {
+      return true;
+    }
+  }
+
+  // check diagonals
+  if board[0][0] == Some(player) && board[1][1] == Some(player) && board[2][2] == Some(player) {
+    return true;
+  }
+
+  if board[0][2] == Some(player) && board[1][1] == Some(player) && board[2][0] == Some(player) {
+    return true;
+  }
+  return false;
 }
 
 #[cfg(test)]
@@ -41,5 +74,70 @@ mod tests {
   fn it_creates_a_board() {
     let board = create_board();
     assert_eq!(board, [[None, None, None], [None, None, None], [None, None, None]]);
+  }
+
+  #[test]
+  fn it_returns_false_if_no_one_has_won() {
+    let board = create_board();
+    assert_eq!(false, has_won(board, Player::X));
+    assert_eq!(false, has_won(board, Player::O));
+  }
+
+  #[test]
+  fn it_returns_true_for_x_if_x_has_won() {
+    let mut board = create_board();
+    board[0][0] = Some(Player::X);
+    board[0][1] = Some(Player::X);
+    board[0][2] = Some(Player::X);
+    assert_eq!(true, has_won(board, Player::X));
+  }
+
+  #[test]
+  fn it_returns_false_for_o_if_x_has_won() {
+    let mut board = create_board();
+    board[0][0] = Some(Player::X);
+    board[0][1] = Some(Player::X);
+    board[0][2] = Some(Player::X);
+    assert_eq!(false, has_won(board, Player::O));
+  }
+
+  // generate a test for a column win for X
+  // generate a test for a column win for O
+  // generate a test for a diagonal win for X
+  // generate a test for a diagonal win for O
+  #[test]
+  fn it_returns_true_for_x_if_x_has_won_in_a_column() {
+    let mut board = create_board();
+    board[0][0] = Some(Player::X);
+    board[1][0] = Some(Player::X);
+    board[2][0] = Some(Player::X);
+    assert_eq!(true, has_won(board, Player::X));
+  }
+
+  #[test]
+  fn it_returns_false_for_o_if_x_has_won_in_a_column() {
+    let mut board = create_board();
+    board[0][0] = Some(Player::X);
+    board[1][0] = Some(Player::X);
+    board[2][0] = Some(Player::X);
+    assert_eq!(false, has_won(board, Player::O));
+  }
+
+  #[test]
+  fn it_returns_true_for_x_if_x_has_won_in_a_diagonal() {
+    let mut board = create_board();
+    board[0][0] = Some(Player::X);
+    board[1][1] = Some(Player::X);
+    board[2][2] = Some(Player::X);
+    assert_eq!(true, has_won(board, Player::X));
+  } 
+
+  #[test]
+  fn it_returns_false_for_o_if_x_has_won_in_a_diagonal() {
+    let mut board = create_board();
+    board[0][0] = Some(Player::X);
+    board[1][1] = Some(Player::X);
+    board[2][2] = Some(Player::X);
+    assert_eq!(false, has_won(board, Player::O));
   }
 }
